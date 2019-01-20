d9e74795c8f91cbb7bcc7dd9b2a29e7d7b3f41a5
raoshihong <379074131@qq.com>
2019-01-20 17:36:52 +0800

    aspect的demo

---
+++ src/main/java/com/rao/study/tools/springboot/aop/TestAspect.java
@@ -0,0 +1,16 @@
+package com.rao.study.tools.springboot.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TestAspect {
+
+    @Before("execution(* com.rao.study.tools.springboot.aop.TestService.*(..))")
+    public void testBefore(){
+        System.out.println("before");
+    }
+
+}
---
+++ src/main/java/com/rao/study/tools/springboot/aop/TestService.java
@@ -0,0 +1,11 @@
+package com.rao.study.tools.springboot.aop;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public void test(){
+        System.out.println("sdfsdf");
+    }
+}
